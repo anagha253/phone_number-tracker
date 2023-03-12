0f707bfba9584d14c79842fe33855d676a8be335
Anagha Mohan <anu2532002@gmail.com>
2023-03-12 10:21:03 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from text import phone
 import phonenumbers
 from phonenumbers import geocoder
+from phonenumbers import carrier
+
 
 first=phonenumbers.parse(phone,"CH")
 print(geocoder.description_for_number(first,"en"))
+first=phonenumbers.parse(phone,"RO")
+print(carrier.name_for_number(first,"en"))
